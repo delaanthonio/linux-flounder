99cdc24694cbbb2dbc36b3bfffdf2dbce42390aa
Dela Anthonio <dell.anthonio@gmail.com>
2017-06-03 13:42:35 -0400

    ota: patchsys: Add audio patch

--- android/ota/boot/patchsys.sh
+++ android/ota/boot/patchsys.sh
@@ -1,5 +1,11 @@
 #!/sbin/sh
 
+# Audio patch
+ui_print "patchsys: applying audio patch"
+mount /vendor
+cp -f "/tmp/rt5677_elf_vad" "/vendor/firmware/rt5677_elf_vad"
+chmod 0644 "/vendor/firmware/rt5677_elf_vad"
+
 # Battery patch
 ui_print "patchsys: applying battery patch"
 cp -f "/tmp/healthd_7.1.1" "/sbin/healthd"
