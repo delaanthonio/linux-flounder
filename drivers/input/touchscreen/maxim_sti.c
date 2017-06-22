b467370ff4effc40e20697c0f74c9eeff94647d3
Dela Anthonio <dell.anthonio@gmail.com>
2017-06-22 17:22:46 -0400

    maxim_sti: Ensure address is initialized before access

--- drivers/input/touchscreen/maxim_sti.c
+++ drivers/input/touchscreen/maxim_sti.c
@@ -1697,7 +1697,7 @@ static void service_irq_legacy_acceleration(struct dev_data *dd)
 static void service_irq(struct dev_data *dd)
 {
 	struct fu_async_data  *async_data;
-	u16                   status, clear, test, address[2], xbuf;
+	u16                   status, clear, test, address[2] = {0}, xbuf;
 	bool                  read_buf[2] = {true, false};
 	int                   ret, ret2;
 
