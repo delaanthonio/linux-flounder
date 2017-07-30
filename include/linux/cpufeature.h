1fc77cf4258f84efda11392544ffc115c069e545
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-29 20:25:50 -0400

    cpufeature: Fix unused const warning

--- include/linux/cpufeature.h
+++ include/linux/cpufeature.h
@@ -44,7 +44,7 @@
  * 'asm/cpufeature.h' of your favorite architecture.
  */
 #define module_cpu_feature_match(x, __init)			\
-static struct cpu_feature const cpu_feature_match_ ## x[] =	\
+static struct cpu_feature const __always_unused cpu_feature_match_ ## x[] =	\
 	{ { .feature = cpu_feature(x) }, { } };			\
 MODULE_DEVICE_TABLE(cpu, cpu_feature_match_ ## x);		\
 								\
